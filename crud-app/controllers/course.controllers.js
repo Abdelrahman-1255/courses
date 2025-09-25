c4562fe73bfb2eec6debd7392c77eed59612c7b5
Abdelrahman <abdelrahmanshaaban125@gmail.com>
2025-09-25 17:43:28 +0300

    standardize all API responses to JSend

--- crud-app/controllers/course.controllers.js
+++ crud-app/controllers/course.controllers.js
@@ -1,8 +1,9 @@
 import { Course } from "../models/courses.model.js";
 import { validationResult } from "express-validator";
+import * as httpStatusText from "../utils/httpStatusText.js";
 export const getAllCourses = async (req, res) => {
   const courses = await Course.find();
-  res.status(200).json(courses);
+  res.status(200).json({ status: httpStatusText.SUCCESS, data: { courses } });
 };
 
 export const getCourseById = async (req, res) => {
@@ -10,23 +11,34 @@ export const getCourseById = async (req, res) => {
     const id = req.params.courseId;
     const course = await Course.findById(id);
     if (!course) {
-      return res.status(404).json({ message: "Course not found" });
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { course: "Course not found" },
+      });
     }
-    res.status(200).json(course);
+    res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
   } catch (error) {
-    res.status(400).json({ message: "Invalid object ID" });
+    res.status(400).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: error.message,
+      code: 400,
+    });
   }
 };
 
 export const addCourse = async (req, res) => {
   const errors = validationResult(req);
-  console.log(errors);
+  // console.log(errors);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({
+      status: httpStatusText.FAIL,
+      data: { errors: errors.array() },
+    });
   }
   const course = new Course(req.body);
   await course.save();
-  res.status(201).json(course);
+  res.status(201).json({ status: httpStatusText.SUCCESS, data: { course } });
 };
 
 export const updateCourse = async (req, res) => {
@@ -36,11 +48,22 @@ export const updateCourse = async (req, res) => {
       { $set: { ...req.body } }
     );
     if (updatedCourse.matchedCount === 0) {
-      return res.status(404).json({ message: "Course not found" });
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { course: "Course not found" },
+      });
     }
-    return res.status(200).json(updatedCourse);
+    return res.status(200).json({
+      status: httpStatusText.SUCCESS,
+      data: { updatedCourse },
+    });
   } catch (error) {
-    return res.status(400).json({ message: error });
+    return res.status(400).json({
+      status: httpStatusText.ERROR,
+      data: null,
+      message: error.message,
+      code: 400,
+    });
   }
 };
 
@@ -48,10 +71,15 @@ export const deleteCourse = async (req, res) => {
   try {
     const deletedCourse = await Course.deleteOne({ _id: req.params.courseId });
     if (deletedCourse.deletedCount === 0) {
-      return res.status(404).json({ message: "Course not found" });
+      return res.status(404).json({
+        status: httpStatusText.FAIL,
+        data: { message: "Course not found" },
+      });
     }
-    return res.status(200).json({ message: "Course deleted successfully" });
+    return res.status(200).json({ status: httpStatusText.SUCCESS, data: null });
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    return res
+      .status(400)
+      .json({ status: httpStatusText.ERROR, data: { message: error.message } });
   }
 };
