f2026924d6a42ed0ff60b81514cbbf854c184bb9
Abdelrahman <abdelrahmanshaaban125@gmail.com>
2025-09-27 00:43:44 +0300

    Refactor: implement asyncWrapper middleware and AppError class for improved error handling

--- crud-app/controllers/course.controllers.js
+++ crud-app/controllers/course.controllers.js
@@ -1,7 +1,9 @@
 import { Course } from "../models/courses.model.js";
 import { validationResult } from "express-validator";
 import * as httpStatusText from "../utils/httpStatusText.js";
-export const getAllCourses = async (req, res) => {
+import { asyncWrapper } from "../middlewares/asyncWrappper.js";
+import AppError from "../utils/appError.js";
+export const getAllCourses = asyncWrapper(async (req, res) => {
   const query = req.query;
   // console.log(query);
   const limit = parseInt(query.limit) || 10;
@@ -9,82 +11,47 @@ export const getAllCourses = async (req, res) => {
   const skip = (page - 1) * limit;
   const courses = await Course.find().limit(limit).skip(skip);
   res.status(200).json({ status: httpStatusText.SUCCESS, data: { courses } });
-};
+});
 
-export const getCourseById = async (req, res) => {
-  try {
-    const id = req.params.courseId;
-    const course = await Course.findById(id);
-    if (!course) {
-      return res.status(404).json({
-        status: httpStatusText.FAIL,
-        data: { course: "Course not found" },
-      });
-    }
-    res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
-  } catch (error) {
-    res.status(400).json({
-      status: httpStatusText.ERROR,
-      data: null,
-      message: error.message,
-      code: 400,
-    });
+export const getCourseById = asyncWrapper(async (req, res, next) => {
+  const id = req.params.courseId;
+  const course = await Course.findById(id);
+  if (!course) {
+    const error = new AppError("Course not found", 404, httpStatusText.FAIL);
+    return next(error);
   }
-};
+  res.status(200).json({ status: httpStatusText.SUCCESS, data: { course } });
+});
 
-export const addCourse = async (req, res) => {
+export const addCourse = asyncWrapper(async (req, res, next) => {
   const errors = validationResult(req);
   // console.log(errors);
   if (!errors.isEmpty()) {
-    return res.status(400).json({
-      status: httpStatusText.FAIL,
-      data: { errors: errors.array() },
-    });
+    const errorMessages = errors.array().map((err) => err.msg);
+    const error = new AppError(errorMessages, 400, httpStatusText.FAIL);
+    return next(error);
   }
   const course = new Course(req.body);
   await course.save();
   res.status(201).json({ status: httpStatusText.SUCCESS, data: { course } });
-};
+});
 
-export const updateCourse = async (req, res) => {
-  try {
-    const updatedCourse = await Course.updateOne(
-      { _id: req.params.courseId },
-      { $set: { ...req.body } }
-    );
-    if (updatedCourse.matchedCount === 0) {
-      return res.status(404).json({
-        status: httpStatusText.FAIL,
-        data: { course: "Course not found" },
-      });
-    }
-    return res.status(200).json({
-      status: httpStatusText.SUCCESS,
-      data: { updatedCourse },
-    });
-  } catch (error) {
-    return res.status(400).json({
-      status: httpStatusText.ERROR,
-      data: null,
-      message: error.message,
-      code: 400,
-    });
-  }
-};
+export const updateCourse = asyncWrapper(async (req, res) => {
+  const updatedCourse = await Course.updateOne(
+    { _id: req.params.courseId },
+    { $set: { ...req.body } }
+  );
+  return res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: { updatedCourse },
+  });
+});
 
-export const deleteCourse = async (req, res) => {
-  try {
-    const deletedCourse = await Course.deleteOne({ _id: req.params.courseId });
-    if (deletedCourse.deletedCount === 0) {
-      return res.status(404).json({
-        status: httpStatusText.FAIL,
-        data: { message: "Course not found" },
-      });
-    }
-    return res.status(200).json({ status: httpStatusText.SUCCESS, data: null });
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ status: httpStatusText.ERROR, data: { message: error.message } });
+export const deleteCourse = asyncWrapper(async (req, res, next) => {
+  const deletedCourse = await Course.deleteOne({ _id: req.params.courseId });
+  if (deletedCourse.deletedCount === 0) {
+    const error = new AppError("Course not found", 404, httpStatusText.FAIL);
+    return next(error);
   }
-};
+  return res.status(200).json({ status: httpStatusText.SUCCESS, data: null });
+});
--- crud-app/index.js
+++ crud-app/index.js
@@ -2,17 +2,39 @@ import express from "express";
 import coursesRoute from "./routes/courses.route.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import * as httpStatusText from "./utils/httpStatusText.js";
+import cors from "cors";
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
-app.use(express.json());
-
-app.use("/api/courses", coursesRoute);
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log("mongodb connected");
+});
+app.use(express.json());
+app.use(cors());
+app.use("/api/courses", coursesRoute);
+
+// Global middleware for not found routes
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: httpStatusText.ERROR,
+    message: "This resource is not available on this server!",
+  });
+  next();
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).json({
+    status: err.statusText || httpStatusText.ERROR,
+    message: err.message || "Internal Server Error",
+    code: err.statusCode || 500,
+    data: null,
+  });
 });
 
 const port = process.env.PORT || 3000;
